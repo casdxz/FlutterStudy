f7efc458a0529565a233a177eca27cfd81b2456f
Bear <19851903253@163.com>
2022-09-13 23:39:58 +0800

    feat: flutterStudy-oneDay

---
+++ flutter_learning/android/app/src/main/kotlin/com/bear/flutter_learning/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.bear.flutter_learning
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ flutter_learning/lib/main.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+import 'pages/profile_one_page.dart';
+
+void main() {
+  runApp(
+    const MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home:
+        ProfileOnePage(),
+    )
+  );
+}
