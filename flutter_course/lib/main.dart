7e5e7edcedc15517bd91688ac22f1f7349fc345b
Bear <19851903253@163.com>
2022-09-23 09:55:19 +0800

    feat: flutterStudy(app store & 圆盘组件)

--- flutter_course/lib/main.dart
+++ flutter_course/lib/main.dart
@@ -1,5 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_course/pages/app_store_page.dart';
+import 'package:flutter_course/pages/event_page.dart';
 import 'package:flutter_course/pages/layout_page.dart';
+import 'package:flutter_course/pages/painter_page.dart';
 
 
 void main() {
@@ -7,7 +10,9 @@ void main() {
       const MaterialApp(
         debugShowCheckedModeBanner: false,
         home:
-        LayoutPage(),
+        // LayoutPage(),
+        // AppStorePage(),
+        PainterPage()
       )
   );
 }
